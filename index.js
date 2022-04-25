42031b2090df2e0a5fa3902c57c39b19114d6b5c
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-04-25 10:14:03 +0530

    Express listen to port

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const {testConn} = require( './db' );
-const Vehicle = require( './models/Vehicle' );
-const Driver = require( './models/Driver' );
-const { sequelize } = require('./models/Driver');
+const app = require( './routes' );
 
-async function initiate() {
-	// Fail first
-	if ( ! await testConn() ) {
-		return;
-	}
+const PORT = 3000;
 
-	// await Vehicle.sync( {alter: true} );
-	// await Driver.sync( {alter: true} );
-
-	await Vehicles.update( {driverId: 1}, { where: {id: 5} } );
-
-	// Work with Vehicle model over here!
-	const result = await Vehicle.findAll( {where: {id:5}, include: Driver} );
-
-	console.log( JSON.parse( JSON.stringify( result ) ) );
-
-}
-
-initiate();
+app.listen( PORT,  () => {
+	console.log( `Listening to port ${PORT}` );
+} );
